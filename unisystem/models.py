b47b95a079c47d4710b54853c85af3f5c4948cb1
Denis Nolting <denis.nolting@gmx.de>
2026-09-24 14:04:19 +0200

    feat(unisystem): add models to modely.py

--- unisystem/models.py
+++ unisystem/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+class Studentenausweis(models.Model):
+    student = models.OneToOneField("Student", on_delete=models.CASCADE)
+    ausweisnummer = models.CharField(max_length=20, unique=True)
+
+class Professor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+class Kurs(models.Model):
+    kurs_name = models.CharField(max_length=50)
+    studenten = models.ManyToManyField("Student")
+    kurs_professor = models.ForeignKey("Professor", on_delete=models.SET_NULL, null=True)
+    semester = models.ForeignKey("Semester", on_delete=models.PROTECT,
+    )
+
+class Kursbeschreibung(models.Model):
+    beschreibung = models.TextField()
+    kurs = models.OneToOneField("Kurs", on_delete=models.CASCADE)
+
+class Semester(models.Model):
+    semester = models.CharField(max_length=30)
